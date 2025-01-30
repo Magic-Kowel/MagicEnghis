88a8871499f298d486a5a46690c6922226f74205
Josue Fierro Morfin <58827128+Magic-Kowel@users.noreply.github.com>
2025-01-29 23:33:46 -0600

    setting file

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import Loader from "./components/Loader.jsx";
 import MainWriteScreen from "./views/MainWriteScreen.jsx";
-import Menu from "./components/Menu/Menu.jsx";
+
+const Menu = lazy(() => import("./components/Menu/Menu.jsx"));
 const NotFound = lazy(() => import("./views/NotFound.jsx"));
 const IrregularsVerbs = lazy(() => import("./views/IrregularsVerbs.jsx"));
 // list path
@@ -16,11 +17,9 @@ function App() {
             key={path}
             path={`/${path}`}
             element={
-              <Suspense fallback={<Loader />}>
-                <Menu>
-                  <MainWriteScreen />
-                </Menu>
-              </Suspense>
+              <Menu>
+                <MainWriteScreen />
+              </Menu>
             }
           />
         ))}
